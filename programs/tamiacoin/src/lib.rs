a813fee92fbc67245bce42f08c51d61daf8954f9
itsJakeChambers <kjeanmarie01@gmail.com>
2025-02-08 19:23:49 +0100

    initialization of the SPL token smart contract

--- programs/tamiacoin/src/lib.rs
+++ programs/tamiacoin/src/lib.rs
@@ -1,16 +1,50 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, MintTo};
 
-declare_id!("vFfDiHaa9toLdP2WUHsjsv6PxM5Wi1sem5gDqgAnr4e");
+declare_id!("");  // program ID
 
 #[program]
-pub mod tamiacoin {
+pub mod my_memecoin {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    // Function to initialize the token
+    pub fn initialize(ctx: Context<Initialize>, decimals: u8, total_supply: u64) -> Result<()> {
+        let mint = &mut ctx.accounts.mint;
+        mint.decimals = decimals;
+        mint.supply = total_supply;
+        Ok(())
+    }
+
+    // Function to "mint" (create) tokens and send them to a user
+    pub fn mint_tokens(ctx: Context<MintTokens>, amount: u64) -> Result<()> {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.recipient.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        token::mint_to(CpiContext::new(cpi_program, cpi_accounts), amount)?;
         Ok(())
     }
 }
 
+// Accounts needed to initiate the token
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, mint::decimals = 9, mint::authority = user)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Accounts needed to "mint" (create) tokens
+#[derive(Accounts)]
+pub struct MintTokens<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub recipient: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+}
